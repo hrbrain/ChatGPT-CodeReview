81a1e5e8cb82a289c4dfa6d92226b21a286c3ae4
Zama Hikaru <zama.hikaru@hrbrain.co.jp>
2024-06-24 10:50:11 +0900

    debug

--- src/chat.ts
+++ src/chat.ts
@@ -68,7 +68,7 @@ Review Format
 
     console.time('code-review cost');
     const prompt = this.generatePrompt(fileExtension, patch);
-    console.log(prompt)
+    console.info(prompt)
 
     const res = await this.chatAPI.sendMessage(prompt);
 
