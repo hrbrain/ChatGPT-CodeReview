c69767c1b787336bb5e18644909441dcd566b905
Hikaru Zama <zama.hikaru@hrbrain.co.jp>
2024-06-25 10:55:01 +0900

    Review when /review commented to PR (#5)

--- src/bot.ts
+++ src/bot.ts
@@ -18,6 +18,14 @@ type PullRequest = {
   url: string;
 };
 
+const noReviewLabels = [
+  'no-review-by-ChatGPT',
+  'renovate/Major',
+  'renovate/Minor',
+  'renovate/Patch',
+  'renovate/security'
+]
+
 export const robot = (app: Probot) => {
   const loadChat = async (context: Context) => {
     if (process.env.OPENAI_API_KEY) {
@@ -53,17 +61,101 @@ export const robot = (app: Probot) => {
   };
 
   const review = async (context: Context, pullRequest: PullRequest) => {
+    if (pullRequest.state === 'closed') {
+      console.log('pull request is closed');
+      return 'pull request is closed';
+    } else if (pullRequest.locked) {
+      console.log('pull request is locked');
+      return 'pull request is locked';
+    } else if (process.env.TRIGGER !== 'command' && pullRequest.labels?.some(label => noReviewLabels.includes(label))) {
+      console.log('no-review label is attached');
+      return 'no-review label is attached'
+    }
+
+    const targets = (process.env.TARGETS || process.env.targets || '')
+      .split(',')
+      .filter((v) => v !== '');
+    if (targets.length === 0) {
+      console.log('no target specified');
+      return 'no target specified';
+    }
+
+    var ignoreList: string[] = []
+    const ignore = process.env.IGNORE || process.env.ignore || '';
+    if (ignore === '') {
+      console.log('no ignore specified');
+      return 'no ignore specified';
+    } else if (ignore !== 'NONE') {
+      ignoreList = ignore
+        .split(',')
+        .filter((v) => v !== '');
+      if (ignoreList.length === 0) {
+        console.log('no ignore specified');
+        return 'no ignore specified';
+      }
+    }
+
     const chat = await loadChat(context);
     if (!chat) {
-      console.log('Chat initialized failed');
-      return 'no chat';
+      console.log('failed to initialize Chat');
+      return 'failed to initialize Chat';
     }
 
     const repo = context.repo();
-    console.info(repo.owner, repo.repo);
-    console.info(pullRequest);
+    const data = await context.octokit.repos.compareCommits({
+      owner: repo.owner,
+      repo: repo.repo,
+      base: pullRequest.baseSha,
+      head: pullRequest.headSha,
+    });
 
-    return 'success'
+    const { files: changedFiles } = data.data;
+    if (!changedFiles?.length) {
+      console.log('no change found');
+      return 'no change found';
+    }
+
+    console.time('gpt cost');
+
+    for (let i = 0; i < changedFiles.length; i++) {
+      const file = changedFiles[i];
+      const patch = file.patch || '';
+      if (file.status !== 'modified' && file.status !== 'added') {
+        console.log(`${file.filename} is not modified or added`);
+        continue;
+      } else if (!targets.some(target => minimatch(file.filename, target))) {
+        console.log(`${file.filename} is not in targets`);
+        continue;
+      } else if (ignoreList.some(ignore => minimatch(file.filename, ignore))) {
+        console.log(`${file.filename} is ignored`);
+        continue;
+      } else if (!patch || patch.length > MAX_PATCH_COUNT) {
+        console.log(`${file.filename} skipped caused by its diff is too large`);
+        continue;
+      }
+
+      try {
+        const res = await chat.codeReview(file.filename, patch);
+        if (!!res) {
+          await context.octokit.pulls.createReviewComment({
+            repo: repo.repo,
+            owner: repo.owner,
+            pull_number: pullRequest.pullNumber,
+            commit_id: pullRequest.headSha,
+            path: file.filename,
+            body: res,
+            position: patch.split('\n').length - 1,
+          });
+        }
+      } catch (e) {
+        console.error(`review ${file.filename} failed`, e);
+      }
+    }
+
+    console.timeEnd('gpt cost');
+    console.info('successfully reviewed', pullRequest.url);
+
+    return 'success';
   };
 
   app.on(
@@ -79,143 +171,26 @@ export const robot = (app: Probot) => {
         locked: pr.locked as boolean,
         url: pr.html_url as string
       };
-      await review(context, pullRequest);
+
+      return await review(context, pullRequest);
     }
   );
 
   app.on(
     ['pull_request.opened', 'pull_request.synchronize'],
     async (context) => {
-      const repo = context.repo();
-      const chat = await loadChat(context);
-
-      if (!chat) {
-        console.log('Chat initialized failed');
-        return 'no chat';
-      }
-
-      const pull_request = context.payload.pull_request;
-
+      const pr = context.payload.pull_request;
       const pullRequest: PullRequest = {
         pullNumber: context.pullRequest().pull_number,
-        baseSha: pull_request.base.sha,
-        headSha: pull_request.head.sha,
-        labels: pull_request.labels?.map(l => l.name),
-        state: pull_request.state,
-        locked: pull_request.locked,
-        url: pull_request.html_url
+        baseSha: pr.base.sha,
+        headSha: pr.head.sha,
+        labels: pr.labels?.map(l => l.name),
+        state: pr.state,
+        locked: pr.locked,
+        url: pr.html_url
       };
-      await review(context, pullRequest);
 
-      if (
-        pull_request.state === 'closed' ||
-        pull_request.locked
-      ) {
-        console.log('invalid event payload');
-        return 'invalid event payload';
-      }
-
-      if (process.env.TRIGGER !== 'command') {
-        const noReviewLabels = [
-          'no-review-by-ChatGPT',
-          'renovate/Major',
-          'renovate/Minor',
-          'renovate/Patch',
-          'renovate/security'
-        ]
-        if (pull_request.labels?.some(label => noReviewLabels.includes(label.name))) {
-          console.log('no-review label is attached.');
-          return 'no-review label is attached.'
-        }
-      }
-
-      const targets = (process.env.TARGETS || process.env.targets || '')
-        .split(',')
-        .filter((v) => v !== '');
-      if (targets.length === 0) {
-        console.log('no target specified');
-        return 'no target specified';
-      }
-
-      var ignoreList: string[] = []
-      const ignore = process.env.IGNORE || process.env.ignore || '';
-      if (ignore === '') {
-        console.log('no ignore specified');
-        return 'no ignore specified';
-      } else if (ignore !== 'NONE') {
-        ignoreList = ignore
-          .split(',')
-          .filter((v) => v !== '');
-        if (ignoreList.length === 0) {
-          console.log('no ignore specified');
-          return 'no ignore specified';
-        }
-      }
-
-      const data = await context.octokit.repos.compareCommits({
-        owner: repo.owner,
-        repo: repo.repo,
-        base: context.payload.pull_request.base.sha,
-        head: context.payload.pull_request.head.sha,
-      });
-
-      const { files: changedFiles, commits } = data.data;
-      if (!changedFiles?.length) {
-        console.log('no change found');
-        return 'no change found';
-      }
-
-      console.time('gpt cost');
-
-      for (let i = 0; i < changedFiles.length; i++) {
-        const file = changedFiles[i];
-        const patch = file.patch || '';
-
-        if (file.status !== 'modified' && file.status !== 'added') {
-          continue;
-        }
-
-        if (!targets.some(target => minimatch(file.filename, target))) {
-          console.log(`${file.filename} is not in targets.`);
-          continue;
-        }
-
-        if (ignoreList.some(ignore => minimatch(file.filename, ignore))) {
-          console.log(`${file.filename} is ignored.`);
-          continue;
-        }
-
-        if (!patch || patch.length > MAX_PATCH_COUNT) {
-          console.log(`${file.filename} skipped caused by its diff is too large.`);
-          continue;
-        }
-
-        try {
-          const res = await chat?.codeReview(file.filename, patch);
-
-          if (!!res) {
-            await context.octokit.pulls.createReviewComment({
-              repo: repo.repo,
-              owner: repo.owner,
-              pull_number: context.pullRequest().pull_number,
-              commit_id: commits[commits.length - 1].sha,
-              path: file.filename,
-              body: res,
-              position: patch.split('\n').length - 1,
-            });
-          }
-        } catch (e) {
-          console.error(`review ${file.filename} failed`, e);
-        }
-      }
-
-      console.timeEnd('gpt cost');
-      console.info(
-        'successfully reviewed',
-        context.payload.pull_request.html_url
-      );
-
-      return 'success';
+      return await review(context, pullRequest);
     }
   );
 };
